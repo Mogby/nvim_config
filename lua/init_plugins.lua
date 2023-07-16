6d411d8485712dab5700a797893bb0fa9536c914
Mikhail Mosiagin <mogbymo@gmail.com>
2023-07-16 16:38:47 +0200

    Ensure packer

--- lua/init_plugins.lua
+++ lua/init_plugins.lua
@@ -1,7 +1,17 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
-local use = require('packer').use
-require('packer').startup(function()
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
   -- Package manager
   use 'wbthomason/packer.nvim'
 
