9254fa41e176762e5a1a92308e18511d6c80cca0
Mikhail Mosiagin <mogbymo@gmail.com>
2023-07-19 22:26:25 +0200

    Create black.lua

---
+++ lua/plugins/black.lua
@@ -0,0 +1,11 @@
+local g = vim.g                 -- global variables
+
+g.black_linelength = 120
+g.black_skip_string_normalization = true
+g.black_quiet = true
+
+vim.api.nvim_create_autocmd({'BufWritePre'}, {
+  pattern='*.py',
+  callback=function()
+    vim.cmd('Black')
+  end
