919b6c229a6b8c451975da7a8e6fdf9f8a1a15d9
Mikhail Mosiagin <mogbymo@gmail.com>
2023-07-25 23:22:08 +0200

    Update nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'json', 'lua', 'python', 'yaml' },
+  ensure_installed = { 'c', 'glsl', 'json', 'lua', 'python', 'yaml' },
   sync_install = false,
   auto_install = true,
 
