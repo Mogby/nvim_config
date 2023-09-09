0cd01b30bf310af4b255f1d14cff139f0cc1687f
Mikhail Mosiagin <mogbymo@gmail.com>
2023-09-09 17:57:32 +0200

    Autoformat on save

--- lua/init_lsp.lua
+++ lua/init_lsp.lua
@@ -52,6 +52,8 @@ lspconfig.lua_ls.setup({
   enableNvimLuaDev = true
 })
 
-vim.diagnostic.config({
-  virtual_text = false
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    vim.lsp.buf.format()
+  end
 })
